eabd134cadacdadc14c1aa809c0065f65df6b448
Tyler Newman <tylerjnewman18@gmail.com>
2020-06-18 17:04:28 -0700

    Add AppBar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from "react";
-import { ThemeProvider } from "@material-ui/core/styles";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import theme from "./theme";
+import React from 'react';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from './theme';
+import NavBar from './NavBar';
 
 const App = () => (
   <ThemeProvider theme={theme}>
     <CssBaseline />
+    <NavBar />
     <div>
       <p>Hello World</p>
     </div>
